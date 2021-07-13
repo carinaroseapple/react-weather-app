18edcb58e4baf239a5d7a7599a4934381462ab62
carinaroseapple <81572187+carinaroseapple@users.noreply.github.com>
2021-07-12 20:23:11 -0400

    updated app.js

---
+++ src/Weather.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./styles.css";
+import Weather from "./Weather";
+
+export default function Weather() {
+  const [City, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "4e9ba2aaf51b6c12486ee31333bf45cc";
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${apiKey}&units=metric`;
+    console.log(apiURL);
+    axios.get(apiURL).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a City..."
+        onChange={updateCity}
+      />
+      <button type="submit" value="Search">
+        {" "}
+        Search{" "}
+      </button>
+    </form>
+  );
+  if (loaded) {
+    return (
+      <div class="searchList">
+        {form}
+        <ul>
+          <li>
+            <strong>{City}</strong>
+          </li>
+          <li>Temperature: {Math.round(weather.temperature)} °C.</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {Math.round(weather.wind)}km/h</li>
+          <li>
+            {" "}
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+function App() {
+  return (
+    <div className="WeatherApp">
+      <h1>Weather App</h1>
+      <Weather />
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
