00c211ea2327516c7b04ca57fd3d6da05c1ac877
carinaroseapple <81572187+carinaroseapple@users.noreply.github.com>
2021-07-15 14:39:26 -0400

    blah

--- src/App.js
+++ src/App.js
@@ -8,15 +8,6 @@ function App() {
     <div className="WeatherApp">
       <h1>Weather App</h1>
       <Weather />
-      <footer>
-        Coded by Carina Easley-Appleyard and
-        <a
-          href="https://github.com/carinaroseapple/react-weather-app.git"
-          target="_blank"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
     </div>
   );
 }
